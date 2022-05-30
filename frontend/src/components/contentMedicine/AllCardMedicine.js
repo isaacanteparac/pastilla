61765561946f0d7d28f73db229f84a0abac9103f
ISAAC ANTEPARA <69361351+isaacanteparac@users.noreply.github.com>
2022-05-29 23:48:12 -0500

    ok

--- frontend/src/components/contentMedicine/AllCardMedicine.js
+++ frontend/src/components/contentMedicine/AllCardMedicine.js
@@ -3,128 +3,67 @@ import CardMedicine from "./CardMedicine";
 
 const data = [
   {
-    name: "hola",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  },
-  {
-    name: "nomo",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  },
-  {
     name: "hola mundo moderno",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
   },
   {
     name: "rata vieja",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
   },
   {
     name: "nomo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
   },
   {
     name: "hola mundo moderno",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
   },
   {
     name: "rata vieja",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
-  }, {
+  },
+  {
     name: "nomo",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
   },
   {
     name: "hola mundo moderno",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
   },
   {
     name: "rata vieja",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  }, {
-    name: "nomo",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  },
-  {
-    name: "hola mundo moderno",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  },
-  {
-    name: "rata vieja",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  }, {
-    name: "nomo",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  },
-  {
-    name: "hola mundo moderno",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
-    levelPain: "4 a 5 de 10",
-  },
-  {
-    name: "rata vieja",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    ages: "50 a 60",
-    weights: "50 a 150",
+    age: "50 a 60",
+    weight: "50 a 150",
     levelPain: "4 a 5 de 10",
   },
 ];
@@ -143,9 +82,15 @@ export default function AllCardMedicine(props) {
     <div className="sectionMedicine">
       <label className="TitleSection">{props.nameSection}</label>
       <div className="contentCard">
-            {medicines?.map((medicine)=>(
-                <CardMedicine nameMedicine={medicine.name} description={medicine.description}/>
-            ))}
+        {medicines?.map((medicine) => (
+          <CardMedicine
+            nameMedicine={medicine.name}
+            description={medicine.description}
+            age={medicine.age}
+            weight={medicine.weight}
+            levelPain={medicine.levelPain}
+          />
+        ))}
       </div>
     </div>
   );
--- frontend/src/components/contentMedicine/CardMedicine.js
+++ frontend/src/components/contentMedicine/CardMedicine.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function CardMedicine(props) {
-    return (
-      <div className='cardMedicine'>
-          <label className='nameMedicine'>{props.nameMedicine}</label>
-          <p className='description'>{props.description}</p>
-          <label className='viewText'>leer mas...</label>
+  return (
+    <div className="cardMedicine">
+      <label className="nameMedicine">{props.nameMedicine}</label>
+      <p className="description">{props.description}</p>
+      <div className="alertMedicine">
+        <label className="subtitle">Edad: </label>
+        <label className="chipAlert">{props.age}</label>
       </div>
-    )
-  
+      <div className="chipAlertMedicine">
+        <label className="subtitle">Peso: </label>
+        <label className="chipAlert">{props.weight}</label>
+      </div>
+      <div className="chipAlertMedicine">
+        <label className="subtitle">Nivel Dolor: </label>
+        <label className="chipAlert">{props.levelPain}</label>
+      </div>
+      <div className="contentBtnDoctor">
+        <button className="btnDoctor">Consultar a medico</button>
+      </div>
+    </div>
+  );
 }
