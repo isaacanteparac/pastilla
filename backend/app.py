35647a5edad606b438b4c5d2b9242b02d101a8b1
ISAAC ANTEPARA <69361351+isaacanteparac@users.noreply.github.com>
2022-07-11 11:04:14 -0500

    oook

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+import MySQLdb
+from flask_mysqldb import MySQL
+
+
 #pip install flask
 #pip install mysql-connector-python
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root_ia'
+app.config['MYSQL_PASSWORD'] = 'root_ia'
+app.config['MYSQL_DB'] = 'syin'
+mysql = MySQL(app)
+
+app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def hello_world():
